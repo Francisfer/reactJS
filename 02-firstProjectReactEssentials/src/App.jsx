70014267194b0a2437ac852e5db8d7ba569ae8a7
Francisfer <114183517+Francisfer@users.noreply.github.com>
2025-02-09 17:04:55 +0000

    Started react essentials.

--- 02-firstProjectReactEssentials/src/App.jsx
+++ 02-firstProjectReactEssentials/src/App.jsx
@@ -1,15 +1,36 @@
+// Import the image instead of hardcoding the src attribute. It is done so that the path does not get lost in the build process and also because we can optimize images.
+import headerImg from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+// Functionality to render a different word on the p element
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+// Parenthesis on the return value because it is multi-line.
+function Header() {
+  // Code to generate the description - here to make the jsx lean.
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
+  return (
+    <header>
+      <img src={headerImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
       <main>
+        <Header /> {/* This is how we "call" our components */}
         <h2>Time to get started!</h2>
       </main>
     </div>
