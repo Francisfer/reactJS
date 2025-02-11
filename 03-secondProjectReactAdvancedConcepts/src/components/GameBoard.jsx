9bff2d124aeec8247b44d973fd395a4bc1efd480
Francisfer <114183517+Francisfer@users.noreply.github.com>
2025-02-11 19:00:14 +0000

    Started second project.

--- 03-secondProjectReactAdvancedConcepts/src/components/GameBoard.jsx
+++ 03-secondProjectReactAdvancedConcepts/src/components/GameBoard.jsx
@@ -1,5 +1,25 @@
 // To make the grid.
-
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 export default function GameBoard() {
-  return <ol id="game-board"></ol>;
+  // The index as key is not a good practice because it is related with the position of the data.
+  // Here it does't matter but keep it in mind.
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((col, colIndex) => (
+              <li key={colIndex}>
+                <button></button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
